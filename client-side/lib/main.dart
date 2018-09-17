515958a75ebebdea3345d8a7944f9c29815ef8a3
ItsNotRick <patrick.cs@knights.ucf.edu>
2018-09-16 23:53:38 -0400

    Combining some work done seperately as part of our 'learning flutter' exercise

--- client-side/lib/main.dart
+++ client-side/lib/main.dart
@@ -6,35 +6,13 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 void test(context) => debugPrint("back");
-Row buildButtonRow(String testText, String path, BuildContext context){
-        
-        // // TODO: figure out what do do for exception handling
-        // if(!(path.startsWith('/') || path.compareTo('pop') == 0)){
-        //   debugPrintStack();
-        //   debugPrint("error: buildButtonRow was not passed a valid pathname");
-        //   exit(0);
-        // }
-
-        return Row(
-          children: [
-            Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  RaisedButton(
-                    onPressed:(path == 'pop') ? () { Navigator.of(context).pop(); } : () { Navigator.of(context).pushNamed(path); },
-                    padding: const EdgeInsets.all(8.0),
-                    textColor: Colors.white,
-                    color: Colors.blue,
-                    child: Text(testText),
-                  ),
-                ]
-              ),
-            ),
-          ],
-        );
-      }
-
+//                  RaisedButton(
+//                    onPressed:(path == 'pop') ? () { Navigator.of(context).pop(); } : () { Navigator.of(context).pushNamed(path); },
+//                    padding: const EdgeInsets.all(8.0),
+//                    textColor: Colors.white,
+//                    color: Colors.blue,
+//                    child: Text(testText),
+//                  ),
 
 class MyApp extends StatelessWidget {
   // build is the main function for a widget.
@@ -42,12 +20,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context){
       return MaterialApp(
-        routes: {
-          // '/': (context) => MyApp(),
-          '/second' : (context) => OptionScreen(),
-        },
         title: 'Flutter Demo',
-        home: MainMenu(),
+        initialRoute: '/',
+        routes: {
+          '/' : (context) => MainMenu(),
+          '/options' : (context) => OptionsMenu(),
+          '/game' : (context) => GameScreen(),
+        },
       );
   }
 
@@ -58,70 +37,131 @@ class MainMenu extends StatelessWidget{
   Widget build(BuildContext context){
 
     Widget titleSection = Container(
-        padding: const EdgeInsets.all(32.0),
-        child: Row(
-          children:[
-            Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Container(
-                    child: Text(
-                      'Welcome to our rhythm game!',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20.0,
-                      ),
-                    ),
-                  ),
-                  Text(
-                    'By Patrick, Matt, Yash, and Chris',
-                    style: TextStyle(
-                      color: Colors.grey[500],
-                    ),
-                  ),
-                ],
-              )
-            )
+      padding: const EdgeInsets.all(32.0),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Welcome to our rhythm game!',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 20.0,
+              ),
+            ),
+            Text(
+              'By Patrick, Matt, Yash, and Chris',
+              style: TextStyle(
+                color: Colors.grey[500],
+              ),
+            ),
           ],
-        ),
-      );
+        )
+      )
+    );
 
     return Scaffold(
       body: ListView(
-        children: <Widget>[
+        children: <Widget> [
           titleSection,
-          buildButtonRow('options', '/second', context),
+          RaisedButton(
+            onPressed:() {
+              Navigator.pushNamed(
+                context,
+                '/game'
+              );
+            },
+            padding: const EdgeInsets.all(8.0),
+            textColor: Colors.white,
+            color: Colors.blue,
+            child: Text('Game'),
+          ),
+          RaisedButton(
+            onPressed:() {
+              Navigator.pushNamed(
+                context,
+                '/options'
+              );
+            },
+            padding: const EdgeInsets.all(8.0),
+            textColor: Colors.white,
+            color: Colors.blue,
+            child: Text('Options'),
+          ),
         ],
       ),
     );
   }
 }
-class OptionScreen extends StatefulWidget{
+class OptionsMenu extends StatefulWidget{
   Options createState() => Options();
 }
 
-class Options extends State<OptionScreen>{
-   double _value = 20.0;
+class Options extends State<OptionsMenu>{
+   double _threshold = 20.0;
   @override
   Widget build(BuildContext context){
     return MaterialApp(
       title: 'Flutter Demo',
       home: Scaffold(
-        body: ListView(
-          children: <Widget>[
-            buildButtonRow('hello', '/', context),
-            Text('Volume',textAlign: TextAlign.center,),
-            Slider(
-              value: _value,
-              min: 0.0,
-              max: 100.0,
-              onChanged: (double value) {
-                setState(() => _value = value);
-              },
-            ),
-          ]
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget> [
+              Text(
+                'Volume',
+              ),
+              Slider(
+                value: _threshold,
+                min: 0.0,
+                max: 100.0,
+                onChanged: (double value) {
+                  setState(() {
+                    _threshold = value;
+                  });
+                },
+              ),
+              RaisedButton(
+                onPressed:() {
+                  Navigator.pop(
+                    context
+                  );
+                },
+                padding: const EdgeInsets.all(8.0),
+                textColor: Colors.white,
+                color: Colors.blue,
+                child: Text('Return'),
+              ),
+            ]
+          ),
         ),
+      ),
+    );
+  }
+}
+
+class GameScreen extends StatefulWidget {
+
+  @override
+  _GameScreenState createState() => _GameScreenState();
+}
+
+class _GameScreenState extends State<GameScreen> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      //appBar: AppBar(
+      //title: Text("Settings"),
+      //),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget> [
+            Text(
+              'game screen :^)'
+            ),
+          ]),
       ),
     );
   }
