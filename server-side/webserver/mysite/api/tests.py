551ba7f80151e96b621e9c5865a1830ae99a6302
Yash <bhosaley5@gmail.com>
2018-10-04 23:48:57 -0400

    added a test for create_user function

--- server-side/webserver/mysite/api/tests.py
+++ server-side/webserver/mysite/api/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
-
+from .models import Player
+from django.contrib.auth.hashers import check_password
+from .views import create_user
 # Create your tests here.
+
+class PlayerTestCase(TestCase):
+    def setUp(self):
+        create_user("user1", "ThisShouldBeHashed")
+        create_user("user2", "yoyoyo")
+    def test_users_created(self):
+        self.assertEqual(check_password("ThisShouldBeHashed", Player.objects.filter(username='user1')[0].pwd_hash ), True )
