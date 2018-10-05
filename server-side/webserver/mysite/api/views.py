8c7234590d64f54f0da3a9d9e3f8337de4c55a20
Yash <bhosaley5@gmail.com>
2018-10-05 19:36:43 -0400

    got file downloads working.. I think

--- server-side/webserver/mysite/api/views.py
+++ server-side/webserver/mysite/api/views.py
@@ -37,8 +37,6 @@ def create_user(u_name, pwd):
 
 def download_song(request):
     song_file = ''
-    
-
 
     if request.method == "GET":
         return HttpResponse('')
@@ -52,32 +50,42 @@ def download_song(request):
     elif Song.objects.filter(name = data['name']).exists() is not True:
         return HttpResponse('Song does not Exist')
     
-    song_file = data['name'].replace('.', '').replace('|', '').replace('*','').replace('?', '').replace('~','') + '.json'
-    
-    with open(os.path.join(os.path.dirname(__file__), "Songs/{}".format(song_file) ) ) as f:
-        song_data = json.load(f)
-    return JsonResponse(song_data)
+    song_file = data['name'].replace('.', '').replace('|', '').replace('*','').replace('?', '').replace('~','') + '.mp3'
+    with open(os.path.join(os.path.dirname(__file__), "Songs/{}".format(song_file) ), "rb" ) as f:
+        song_data = f.read()
+    response = HttpResponse(song_data, content_type='application/mp3')
+    response['Content-Disposition'] = 'attachment; filename="song.mp3"'
+    return response
 
-def record_play(request):
-    if request.method == "GET":
-        return HttpResponse('')
-
-    data = json.loads(request.body)
     
-    if data['username'] == '' or Player.objects.filter(username= request.POST['username'] ).exists() is not True:
-        return HttpResponse("invalid username")
-    # should we check a password for validation?
-    elif data['beatmap'] == '' or BeatMap.objects.filter(id= request.POST['beatmap'] ).exists() is not True:
-        return HttpResponse('invalid beat_map')
-    elif data['score'] == '':
-        return HttpResponse('empty score')
     
-    if data['rating'] == '':
-        rating = None
-    create_play(request.POST['username'],  request.POST['beatmap'], request.POST['score'],request.POST['rating'] )
 
-def create_play(username, beatmap, score, rating):
-    new_play = Play(player = username, beat_map = beatmap, score = score, rating = rating)
+# download_beatmap(request):
+# with open(os.path.join(os.path.dirname(__file__), "Songs/{}".format(song_file) ) ) as f:
+#         song_data = json.load(f)
+#     return JsonResponse(song_data)
+
+
+# def record_play(request):
+#     if request.method == "GET":
+#         return HttpResponse('')
+
+#     data = json.loads(request.body)
+    
+#     if data['username'] == '' or Player.objects.filter(username= data['username'] ).exists() is not True:
+#         return HttpResponse("invalid username")
+#     # should we check a password for validation?
+#     elif data['beatmap'] == '' or BeatMap.objects.filter(id= data['beatmap'] ).exists() is not True:
+#         return HttpResponse('invalid beat_map')
+#     elif data['score'] == '':
+#         return HttpResponse('empty score')
+    
+#     if data['rating'] == '':
+#         rating = None
+#     create_play(data['username'],  data['beatmap'], data['score'],data['rating'] )
+
+# def create_play(username, beatmap, score, rating):
+#     new_play = Play(player = username, beat_map = beatmap, score = score, rating = rating)
 
     
     
