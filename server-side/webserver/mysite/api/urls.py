82c64eab86f823229e1bd5744a616dee5cb946bb
Yash <bhosaley5@gmail.com>
2018-10-04 23:18:24 -0400

    connecting the 'download_song' function to an actual url

--- server-side/webserver/mysite/api/urls.py
+++ server-side/webserver/mysite/api/urls.py
@@ -4,5 +4,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('register', views.register, name='register')
+    path('register', views.register, name='register'),
+    path('download_song', views.download_song, name='download_song')
 ]
