d8862d25981cbfd45f0fd18c9ea76138798f5220
Yash <bhosaley5@gmail.com>
2018-10-04 23:20:38 -0400

    added PASSWORD_HASHERS for easy hasher access

--- server-side/webserver/mysite/mysite/settings.py
+++ server-side/webserver/mysite/mysite/settings.py
@@ -99,6 +99,13 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+PASSWORD_HASHERS = [
+    'django.contrib.auth.hashers.BCryptPasswordHasher',
+    'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+    'django.contrib.auth.hashers.Argon2PasswordHasher',
+]
 
 
 # Internationalization
