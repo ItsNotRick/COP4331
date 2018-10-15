eb75966b2fe18a39d5c98a0e236c27e23239bc6f
Yash <bhosaley5@gmail.com>
2018-10-14 22:26:56 -0400

    more testing as well as more functional code

--- server-side/webserver/mysite/api/tests.py
+++ server-side/webserver/mysite/api/tests.py
@@ -1,8 +1,9 @@
 from django.test import TestCase, Client
-from .models import Player
+from .models import Player, Song, BeatMap
 from django.contrib.auth.hashers import check_password
 from .views import create_user
 import json
+import os
 # Create your tests here.
 
 class PlayerTestCase(TestCase):
@@ -44,3 +45,65 @@ class PlayerTestCase(TestCase):
         self.assertEqual(response2.content, b"duplicate user.")
         
         self.assertEqual(Player.objects.filter(username = "dupe").count(), 1)
+
+class RecordBeatMapTestCase(TestCase):
+    def setUp(self):
+        fish = Song(name="Fish", artist=None, bpm=180, genre='Pop', timeSig=4)
+        fish.save()
+    
+    def test_normal_working(self):
+        
+        c = Client()
+        
+        body = {"name" : "Fish"}
+        response = c.post("/api/download_beatmap", json.dumps(body), content_type="application/json")
+        self.assertEqual(response.status_code,200)
+        
+        with open(os.path.join(os.path.dirname(__file__), "Songs/{}".format("Fish.json") ) ) as f:
+            song_data = json.load(f)
+
+        compareVal = json.loads(response.content.decode('utf-8'))
+        
+        self.assertEqual(song_data, compareVal)
+        
+    def test_wrong_name(self):
+        c = Client()
+        
+        body = {"name" : "Turtle"}
+        response = c.post("/api/download_beatmap", json.dumps(body), content_type="application/json")
+        self.assertEqual(response.status_code,200)
+        self.assertEqual(response.content, b"Song Turtle does not Exist")
+    
+    def test_no_name(self):
+        c = Client()
+        
+        body = {"" : "lol"}
+        response = c.post("/api/download_beatmap", json.dumps(body), content_type="application/json")
+        self.assertEqual(response.status_code,200)
+        self.assertEqual(response.content, b"empty name")
+
+class RecordBeatMapTestCase(TestCase):
+    def setUp(self):
+        test_user = Player(username = 'Yash', pwd_hash="sdkjfsldkjf")
+        test_song = Song(name="Fish", artist=None, bpm=180, genre='Pop', timeSig=4)
+        test_map = BeatMap(difficulty=1, creator=1, Song=1) 
+        
+        test_user.save()
+        test_song.save()
+        test_map.save()
+
+    def test_normal_working(self):
+        c = Client()
+
+        body = { "username" : "Yash", "beatmap": "1", "score" : "100" }
+        response = c.post('/api/record_beatmap',  json.dumps(body), content_type="application/json")
+        
+        self.assertEqual(response.status_code, 200)
+        compareVal = json.loads(response.content.decode('utf-8'))
+        self.assertEqual(compareVal, {"message": "Score Registered!"})
+
+        
+    def test_missing_fields(self):
+        pass
+    def test_negative_score(self):
+        pass
--- server-side/webserver/mysite/api/urls.py
+++ server-side/webserver/mysite/api/urls.py
@@ -5,5 +5,6 @@ from django.urls import include, path
 urlpatterns = [
     path('', views.index, name='index'),
     path('register', views.register, name='register'),
-    path('download_song', views.download_song, name='download_song')
+    path('download_song', views.download_song, name='download_song'),
+    path('download_beatmap', views.download_beatmap, name='download_beatmap')
 ]
