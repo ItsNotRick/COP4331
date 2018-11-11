96186e29ebff623ecda3af043d11b5006b99737f
MattNipper <MattNipper@knights.ucf.edu>
2018-11-10 23:38:21 -0500

    Added functionality to play music on the test page
    Please add an mp3 audio file named TestSong.mp3 to ..\COP4331\client-side\songs before running this application

--- client-side/lib/animation.dart
+++ client-side/lib/animation.dart
@@ -1,5 +1,20 @@
+import 'dart:async';
+import 'dart:io';
+import 'dart:typed_data';
+
+
 import 'package:flutter/material.dart';
 import 'package:flutter/animation.dart';
+import 'package:audioplayer/audioplayer.dart';
+import 'package:http/http.dart';
+import 'package:path_provider/path_provider.dart';
+
+typedef void OnError(Exception exception);
+
+const kUrl = "http://www.rxlabz.com/labz/audio2.mp3";
+const kUrl2 = "http://www.rxlabz.com/labz/audio.mp3";
+
+enum PlayerState { stopped, playing, paused }
 
 class TestScreen extends StatefulWidget {
   @override
@@ -11,6 +26,142 @@ class _TestScreenState extends State<TestScreen> with TickerProviderStateMixin {
   AnimationController _controller2;
   int score = 1;  
   bool _visible = true;
+
+
+  ////////////////////
+  // Audio Controls //
+  ////////////////////
+
+  Duration duration;
+  Duration position;
+
+  AudioPlayer audioPlayer;
+
+  String localFilePath;
+
+  PlayerState playerState = PlayerState.stopped;
+
+  get isPlaying => playerState == PlayerState.playing;
+  get isPaused => playerState == PlayerState.paused;
+
+  get durationText =>
+      duration != null ? duration.toString().split('.').first : '';
+  get positionText =>
+      position != null ? position.toString().split('.').first : '';
+
+  bool isMuted = false;
+
+  StreamSubscription _positionSubscription;
+  StreamSubscription _audioPlayerStateSubscription;
+
+  @override
+  void dispose() {
+    _positionSubscription.cancel();
+    _audioPlayerStateSubscription.cancel();
+    audioPlayer.stop();
+    super.dispose();
+  }
+
+  void initAudioPlayer() {
+    audioPlayer = new AudioPlayer();
+    _positionSubscription = audioPlayer.onAudioPositionChanged.listen(
+            (p) => setState(() => position = p)
+    );
+    _audioPlayerStateSubscription = audioPlayer.onPlayerStateChanged.listen((s) {
+      if (s == AudioPlayerState.PLAYING) {
+        setState(() => duration = audioPlayer.duration);
+      } else if (s == AudioPlayerState.STOPPED) {
+        onComplete();
+        setState(() {
+          position = duration;
+        });
+      }
+    }, onError: (msg) {
+      setState(() {
+        playerState = PlayerState.stopped;
+        duration = new Duration(seconds: 0);
+        position = new Duration(seconds: 0);
+      });
+    });
+  }
+
+  Future play() async {
+    await audioPlayer.play(kUrl);
+    setState(() {
+      playerState = PlayerState.playing;
+    });
+  }
+
+  Future _playLocal() async {
+    if(localFilePath == null)
+      print('local file path is null');
+    else
+    {
+      await audioPlayer.play(localFilePath, isLocal: true);
+      setState(() => playerState = PlayerState.playing);
+    }
+  }
+
+  Future pause() async {
+    await audioPlayer.pause();
+    setState(() => playerState = PlayerState.paused);
+  }
+
+  Future stop() async {
+    await audioPlayer.stop();
+    setState(() {
+      playerState = PlayerState.stopped;
+      position = new Duration();
+    });
+  }
+
+  Future mute(bool muted) async {
+    await audioPlayer.mute(muted);
+    setState(() {
+      isMuted = muted;
+    });
+  }
+
+  void onComplete() {
+    setState(() => playerState = PlayerState.stopped);
+  }
+
+  Future<Uint8List> _loadFileBytes(String url, {OnError onError}) async {
+    Uint8List bytes;
+    try {
+      bytes = await readBytes(url);
+    } on ClientException {
+      rethrow;
+    }
+    return bytes;
+  }
+
+  Future playSong() async{
+    await _loadFile();
+    _playLocal();
+  }
+
+  Future _loadFile() async{
+
+    final file = new File('${(await getTemporaryDirectory()).path}/music.mp3');
+    await file.writeAsBytes((await (DefaultAssetBundle.of(context)).load("songs/TestSong.mp3")).buffer.asUint8List());
+    if (await file.exists())
+    {
+      String value = file.path;
+      print('The file does exist at: $value');
+      setState(() {
+        return localFilePath = file.path;
+      });
+    }
+    else
+      print('The file didn\'t exist');
+
+  }
+
+  ////////////////
+  // The Widget //
+  ////////////////
+
   initState() {
     super.initState();
     {
@@ -22,6 +173,10 @@ class _TestScreenState extends State<TestScreen> with TickerProviderStateMixin {
 
       _controller.forward();
       _controller2.forward();
+
+      initAudioPlayer();
+      playSong();
+
     }
   }
 
@@ -42,11 +197,16 @@ class _TestScreenState extends State<TestScreen> with TickerProviderStateMixin {
               textAlign: TextAlign.center,
             ),
             
-            
+
+
+
             body: new Center(
                 child: Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
+
+                //should music initialization go here?
+
 
                 ScaleTransition(
                     scale: CurvedAnimation(
